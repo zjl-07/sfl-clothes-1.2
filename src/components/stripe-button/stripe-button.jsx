6ca9c9c8fd33c1a820cf00765cb4e84529300af2
EmildaZhang <emildazhg07@gmail.com>
2019-10-04 14:25:46 +0700

    up to heroku

--- src/components/stripe-button/stripe-button.jsx
+++ src/components/stripe-button/stripe-button.jsx
@@ -1,29 +1,12 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
-import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForTripe = price * 100;
   const publishableKey = "pk_test_cg5tQc24gZTGLbdoj1S2D4zx00uJEuPBsT";
 
   const onToken = token => {
-    axios({
-      url: "payment",
-      method: "post",
-      data: {
-        amount: priceForTripe,
-        token: token
-      }
-    })
-      .then(response => {
-        alert("succesful payment");
-      })
-      .catch(error => {
-        console.log("Payment Error: ", JSON.parse(error));
-        alert(
-          "There was an issue with your payment! Please make sure you use the provided credit card."
-        );
-      });
+    alert("payment success!");
   };
 
   return (
